6815ed502f9483f70b3f59a857bf9a12a99b1a14
user <keithhigginsowns@gmail.com>
2018-04-02 16:52:53 +0100

    Added sounds to assets folder

---
+++ UltimateFifaApp/Model/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateFifaApp.Model
+{
+    class Sound
+    {
+        public String Name { get; set; }
+        public SoundCatagory Catagory { get; set; }
+        public String AudioFile { get; set; }
+        public String ImageFile { get; set; }
+    }
+
+    public enum SoundCatagory
+    {
+        Bad,
+        Fifa Comm,
+        Good
+    }
+}
